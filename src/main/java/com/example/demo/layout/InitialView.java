d56f959bcf04aac5dcf0f184084626b64a1c2e76
newuserrepo <newuserrepo@gmail.com>
2018-11-24 21:37:11 +0100

    testEasyUpload

--- src/main/java/com/example/demo/layout/InitialView.java
+++ src/main/java/com/example/demo/layout/InitialView.java
@@ -1,20 +1,16 @@
 package com.example.demo.layout;
 
 import com.example.demo.MyUI;
-import com.example.demo.calculateHashes.processAsync.ProcessAsync;
 import com.example.demo.util.ShowData;
 import com.example.demo.util.event.MyEventBus;
 import com.google.common.eventbus.Subscribe;
 import com.vaadin.navigator.View;
 import com.vaadin.shared.ui.ContentMode;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
+import org.vaadin.easyuploads.MultiFileUpload;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.attribute.DosFileAttributes;
+import java.io.File;
 
 public class InitialView extends VerticalLayout implements View {
 
@@ -28,13 +24,23 @@ public class InitialView extends VerticalLayout implements View {
         label.addStyleName("h2");
         label.setContentMode(ContentMode.HTML);
         label.setValue(getPhrase());
-        addComponents(label);
 
+        final MultiFileUpload upload = upload();
+
+        addComponents(label, upload);
+        setExpandRatio(upload,1);
         MyEventBus.register(this);
-
     }
 
+    private MultiFileUpload upload() {
+        final MultiFileUpload multiFileUpload = new MultiFileUpload() {
+            @Override
+            protected void handleFile(File tmpFile, String fileName, String mimeType, long length) {
 
+            }
+        };
+        return multiFileUpload;
+    }
 
     @Subscribe
     public void update(final String count) {
